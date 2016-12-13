736cbbde9a441374892580af6e2f18792496afff
Yoshi <thangnv77@gmail.com>
2016-12-14 00:01:03 +0700

    Add boolean schema test cases

---
+++ test/boolean.spec.ts
@@ -0,0 +1,26 @@
+import 'mocha';
+import assert = require('assert');
+import { BooleanSchema } from '../src/schemas/boolean';
+
+class Schema extends BooleanSchema<boolean, boolean> {}
+
+describe('Boolean schema test', () => {
+  it('Should create simple boolean schema correctly', () => {
+    const schema = new Schema();
+    assert.deepEqual(schema.getJsonSchema(), { type: 'boolean'});
+  })
+
+  it('Should interact with nullable & optional correctly', () => {
+    const schema = new Schema();
+    assert.deepEqual(schema.getJsonSchema(), { type: 'boolean'});
+    assert.deepEqual(schema.nullable().getJsonSchema(), { type: 'boolean', 'x-nullable': true });
+    assert.deepEqual(schema.nullable().notNullable().getJsonSchema(), { type: 'boolean', 'x-nullable': false });
+    assert.deepEqual(schema.notNullable().getJsonSchema(), { type: 'boolean', 'x-nullable': false });
+    assert.deepEqual(schema.optional().getJsonSchema(), { type: 'boolean', 'x-optional': true });
+    assert.deepEqual(schema.required().getJsonSchema(), { type: 'boolean', 'x-optional': false });
+    assert.deepEqual(
+      schema.nullable().optional().getJsonSchema(),
+      { type: 'boolean', 'x-optional': true, 'x-nullable': true },
+    );
+  })
+});
