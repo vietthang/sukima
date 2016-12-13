3842c53cae9e3bd43077ecf1ef1ac6a0ad214471
Yoshi <thangnv77@gmail.com>
2016-12-14 00:27:10 +0700

    Refactor boolean test case

--- test/boolean.spec.ts
+++ test/boolean.spec.ts
@@ -2,16 +2,16 @@ import 'mocha';
 import assert = require('assert');
 import { BooleanSchema } from '../src/schemas/boolean';
 
-class Schema extends BooleanSchema<boolean, boolean> {}
+class SchemaImpl extends BooleanSchema<boolean, boolean> {}
 
 describe('Boolean schema test', () => {
   it('Should create simple boolean schema correctly', () => {
-    const schema = new Schema();
+    const schema = new SchemaImpl();
     assert.deepEqual(schema.getJsonSchema(), { type: 'boolean'});
   })
 
   it('Should interact with nullable & optional correctly', () => {
-    const schema = new Schema();
+    const schema = new SchemaImpl();
     assert.deepEqual(schema.getJsonSchema(), { type: 'boolean'});
     assert.deepEqual(schema.nullable().getJsonSchema(), { type: 'boolean', 'x-nullable': true });
     assert.deepEqual(schema.nullable().notNullable().getJsonSchema(), { type: 'boolean', 'x-nullable': false });
